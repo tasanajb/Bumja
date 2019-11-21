8c5c416351b28f0dd38ec285a6fcb2300f1cfecc
tasanajb <50372001+tasanajb@users.noreply.github.com>
2019-11-22 04:06:54 +0700

    Add files via upload

---
+++ bum5.js
@@ -0,0 +1,64 @@
+import React,{Component} from 'react';
+import './Bum.css';
+import Footer from './footer';
+import Adheader from './appheader';
+
+class bum5 extends Component {
+
+render(){
+    
+        return (
+          <div className="App" >
+      
+
+
+
+       <Adheader/>
+      
+<div class="box">
+
+<ul>
+  <li><a class="active" href="#home"><b>หมวดหมู่</b></a></li>
+  <li><a href="#contact"><b>&nbsp;&nbsp;&nbsp;&nbsp;สุนัข&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</b></a></li>
+  <li><a href="#about"><b>&nbsp;&nbsp;&nbsp;&nbsp;แมว&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</b></a></li>
+  <li><a href="#about"><b>&nbsp;&nbsp;&nbsp;&nbsp;อื่นๆ&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</b></a></li>
+  <li><a href="#news"><b>&nbsp;&nbsp;&nbsp;&nbsp;พื้นที่&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-</b>
+  <br/>&nbsp;&nbsp;&nbsp;&nbsp;ทั้งหมด
+  <br/>&nbsp;&nbsp;&nbsp;&nbsp;กรุงเทพมหานคร
+  <br/>&nbsp;&nbsp;&nbsp;&nbsp;สมุทรปราการ</a></li>
+</ul>
+       <body>
+      
+       
+       
+      </body> 
+      <div class="center">
+        <div class="pagination">
+        <a href="#">Back</a>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">...</a>
+        <a href="#">20</a>
+        <a href="#">Next</a>
+        </div>
+
+        
+
+        </div>
+      
+      </div> 
+
+      </div> 
+        ); 
+        
+      }
+    }
+      
+      export default bum5;
+
